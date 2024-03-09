3cc628fda887f300222b903fc850c96ee86ab53f
hzoells <hzoells@gmail.com>
2024-03-09 00:06:35 -0600

    clear console

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { returnArg } from 'index'
-
-describe('Return Args', () => {
-  test('returns arg', () => {
-    const arg = 2
-    expect(returnArg(arg)).toBe(arg)
-  })
-})
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-export const helloWorld = () => {
-  console.log('Hello, world!')
+import { Transform, pipeline } from 'stream'
+import { stdin } from 'node:process'
+
+const SIGINT = 3
+
+const handleExit = new Transform({
+  transform: (data, encoding, callback) => {
+    if (data.toString().charCodeAt(0) === SIGINT) {
+      callback(null)
+      stdin.destroy()
+      return
+    }
+    callback(null, data)
+  }
+})
+
+const main = async () => {
+  console.log('\x1Bc')
+  stdin.setRawMode(true)
+
+  await pipeline(stdin, handleExit, (err) => {})
 }
 
-export const returnArg = (arg: number) => {
-  return arg
-}
+main()
 
-helloWorld()
